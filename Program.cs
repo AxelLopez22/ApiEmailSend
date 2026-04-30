392d8111004a1cb1e04141545421699fc60242c4
AxelLopez22 <81932626+AxelLopez22@users.noreply.github.com>
2026-04-29 22:03:51 -0600

    Enable Swagger in all environments

--- Program.cs
+++ Program.cs
@@ -19,11 +19,10 @@ builder.Services.Configure<List<ClienteConfig>>(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseAuthorization();
 
